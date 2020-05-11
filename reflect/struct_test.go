4c96a86e557d189a5c93f2a1eeea6bddd3972fa7
cuiguangsong_dxm <cuiguangsong@duxiaoman.com>
2020-05-11 09:46:53 +0800

    reflect

--- reflect/struct_test.go
+++ reflect/struct_test.go
@@ -1,6 +1,7 @@
 package reflect
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -14,23 +15,43 @@ import (
 */
 
 type user struct {
-
 	name string
-	age int
+	age  int
 }
-
 
 type manager struct {
 	user
 	title string
 }
-func TestStruct(t *testing.T){
 
-	//var m manager
-	//ty:=reflect.TypeOf(&m)
+func TestStruct(t *testing.T) {
 
+	var m manager
+	ty := reflect.TypeOf(&m)
 
-	//if ty.Kind()
+	//获取指针的基类型
+	if ty.Kind() == reflect.Ptr {
+		ty = ty.Elem()
+	}
 
+	for i := 0; i < ty.NumField(); i++ {
+		f := ty.Field(i)
+
+		fmt.Println(f.Name, f.Type, f.Offset)
+
+		//输出匿名字段结构
+		if f.Anonymous {
+
+			for x := 0; x < f.Type.NumField(); x++ {
+
+				af := f.Type.Field(x)
+
+				fmt.Println(" ", af.Name, af.Type)
+
+			}
+
+		}
+
+	}
 
 }
